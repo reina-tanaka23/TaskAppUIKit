41bbb4e8b1ad407c357fc1d254357d4a894c7d5c
reina.tanaka <reina.tanaka0223@gmail.com>
2021-04-07 13:58:51 +0900

    install Realm

---
+++ TaskAppUIKit/InputViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InputViewController.swift
+//  TaskAppUIKit
+//
+//  Created by reina.tanaka on 2021/04/07.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
